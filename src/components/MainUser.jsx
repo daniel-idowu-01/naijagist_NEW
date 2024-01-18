487738e220a4a654975cbde31764dcc5b9f5fd05
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-19 00:02:06 +0100

    edited width of chat comps

--- src/components/MainUser.jsx
+++ src/components/MainUser.jsx
@@ -4,7 +4,7 @@ const MainUser = () => {
 
   return (
     <article
-      className='mt-5 md:mt-0 flex items-start gap-2 p-4 w-full md:w-1/2 bg-[#60B7FF] text-white rounded-2xl rounded-br-none justify-between'
+      className='mt-5 md:mt-0 flex items-start gap-2 p-4 w-9/12 md:w-1/2 bg-[#60B7FF] text-white rounded-2xl rounded-br-none justify-between'
     >
       <div>
         <p className='text-[#93ED93]'>You: </p>
--- src/components/OtherUser.jsx
+++ src/components/OtherUser.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OtherUser = () => {
 
   return (
-      <article className='mt-5 md:mt-0 flex items-start gap-2 p-4 md:w-1/2 bg-teal-400 text-white rounded-2xl rounded-bl-none'>
+      <article className='mt-5 md:mt-0 flex items-start gap-2 p-4 w-9/12 md:w-1/2 bg-teal-400 text-white rounded-2xl rounded-bl-none'>
         <img src='' alt="" className='hidden md:block bg-[#3978D4] p-2 rounded-[100%]' />
         <div>
           <p className='text-[#E1B023]'>Other User:</p>
