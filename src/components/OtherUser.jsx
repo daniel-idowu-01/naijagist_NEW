b8fb53a53bdfebc9af2d394ee14d20e2bf58532a
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-18 23:39:43 +0100

    created other user comp

--- src/components/OtherUser.jsx
+++ src/components/OtherUser.jsx
@@ -1,18 +1,14 @@
-import React, { useContext } from 'react'
-import { LogoWhite } from '../assets/images'
-import NavContext from '../context/NavContext.jsx'
+import React from 'react'
 
 const OtherUser = () => {
-
-  const { botResponse, setBotResponse } = useContext(NavContext)
 
   return (
       <article className='mt-5 md:mt-0 flex items-start gap-2 p-4 md:w-1/2 bg-white rounded-2xl rounded-bl-none'>
         <img src={LogoWhite} alt="" className='hidden md:block bg-[#3978D4] p-2 rounded-[100%]' />
         <div>
-          <p className='text-[#E1B023]'>AI Doc</p>
+          <p className='text-[#E1B023]'>Other User:</p>
           <p>
-            {botResponse}
+            Hello
           </p>
         </div>
       </article>
