463adfabc7e13e0246284fca4052f47eb819abbe
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-20 18:51:36 +0100

    added profile and cover images in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ProfileComp from '../components/ProfileComp'
 
 const Profile = () => {
   return (
-    <div>
-      Profile
+    <div className='relative md:top-20'>
+      <ProfileComp />
     </div>
   )
 }
